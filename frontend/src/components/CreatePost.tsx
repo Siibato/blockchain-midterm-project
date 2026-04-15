acddffada879e06664682ca847adbfe5095ecd23
Siibato <mykiell5153@gmail.com>
2026-04-15 14:56:06 +0800

    fix build errors

--- frontend/src/components/CreatePost.tsx
+++ frontend/src/components/CreatePost.tsx
@@ -1,4 +1,5 @@
-import { useState, FormEvent } from "react";
+import { useState } from "react";
+import type { FormEvent } from "react";
 import { useContract } from "../hooks/useContract";
 import { usePosts } from "../hooks/usePosts";
 import type { TxStatus } from "../types";
